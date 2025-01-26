aa67c22eb059e52000af7c94e8b7be62834ba66b
ATE <akashsriyt@gmail.com>
2025-01-26 19:49:00 +0530

    fixed footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -12,12 +12,20 @@ function Footer() {
   <footer className="py-12">
         <div className="max-w-screen-2xl container mx-auto px-4 md:px-20">
             <div className="flex flex-col items-center justify-center"> 
-                <div className="flex space-x-4 rounded-lg">
-                    <FaSquareGithub size={24}/>
-                    <FaLinkedin size={24}/>
-                    <FaInstagramSquare size={24}/>
-                    <FaSquareXTwitter size={24}/>
-                </div>
+            <ul className="flex space-x-5">
+                  <li>
+                    <a href="https://github.com/akash-srivastav02" target="_blank"><FaSquareGithub className="text-2xl cursor-pointer"/></a>
+                  </li>
+                  <li>
+                    <a href="https://www.linkedin.com/in/akash-srivastav02/" target="_blank"><FaLinkedin className="text-2xl cursor-pointer"/></a>
+                  </li>
+                  <li>
+                    <a href="https://www.instagram.com/codesskyy/" target="_blank"><FaInstagramSquare className="text-2xl cursor-pointer" /></a>
+                  </li>
+                  <li>
+                    <a href="https://x.com/sky_srivastav" target="_blank"><FaSquareXTwitter className="text-2xl cursor-pointer"/></a>
+                  </li>
+               </ul>
                 <div className="mt-8 border-t border-gray-700 pt-8 flex flex-col items-center">
                     <p className="text-sm">
                         &copy; 2025 Akash Srivastav. All rights reserved.
